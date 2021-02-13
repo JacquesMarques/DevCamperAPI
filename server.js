8909c490e19874ad9989f4026c411273f7915a5e
jacques <jmarques1@gmail.com>
2021-02-13 12:30:44 -0300

    MongoDB setup initiated

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env'});
 
+// Connect to database
+connectDB();
+
+// Router files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Dev loggin middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
+// Mount rputers
+app.use('/api/v1/bootcamps', bootcamps);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(
     PORT,
