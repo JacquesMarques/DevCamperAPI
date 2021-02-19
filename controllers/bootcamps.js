dbf17f8ec6b29daef0ca5939ada300863a0567bd
jacques <jmarques1@gmail.com>
2021-02-19 08:03:06 -0300

    Get Bootcamps within distance of zipcode

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('../middleware/async');
 const ErrorResponse = require('../utils/errorResponse');
+const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc    Get all bootcamps
@@ -66,5 +67,33 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     }
 
     res.status(200).json({success: true, data: {}});
+});
+
+
+// @desc    Get bootcamps within a radius
+// @route   DELETE /api/v1/bootcamps/radius/:zipcod/:distance
+// @access  Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+    const { zipcode, distance } = req.params;
+
+    // Get lat/lng from geocoder
+    const loc = await geocoder.geocode(zipcode);
+    const lat = loc[0].latitude;
+    const lng = loc[0].longitude;
+
+    // Calca radius using radians
+    // Divide distance by radius of Earth
+    // Earth Radius = 3,963 mi / 6.378 Km
+    const radius = distance / 3963;
+
+    const bootcamps = await Bootcamp.find({
+        location: { $geoWithin: { $centerSphere: [[ lng, lat], radius ] } }
+    });
+
+    res.status(200).json({
+        success: true,
+        count: bootcamps.length,
+        data: bootcamps
+    });
 });
 
