603cc1759d129288bb954558718f94bfd9cc1712
jacques <jmarques1@gmail.com>
2021-03-23 09:36:24 -0300

    Feat: Admin Users CRUD

---
+++ routes/users.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const {
+    getUsers,
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../controllers/users');
+
+
+const User = require('../models/User');
+const advancedResults = require('../middleware/advancedResults');
+
+
+const router = express.Router({ mergeParams: true });
+
+const { protect, authorize } = require('../middleware/auth');
+
+router.use(protect);
+router.use(authorize( 'admin'));
+
+router
+    .route('/')
+        .get(advancedResults(User, {
+                path: 'user',
+                select: 'name email'
+            }),
+            getUsers
+        )
+        .post(createUser);
+
+router
+    .route('/:id')
+        .get(getUser)
+        .put(updateUser)
+        .delete(deleteUser);
+
+module.exports = router;
