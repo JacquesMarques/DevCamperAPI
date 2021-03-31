f96d45c5e39e41ede98be42c384d0f7f1d2438b1
jacques <jmarques1@gmail.com>
2021-03-31 07:21:25 -0300

    Reviews controller and route

---
+++ controllers/reviews.js
@@ -0,0 +1,23 @@
+const asyncHandler = require('../middleware/async');
+const ErrorResponse = require('../utils/errorResponse');
+const Review = require('../models/Review');
+const Bootcamp = require('../models/Bootcamp');
+
+// @desc    Get all reviews
+// @route   GET /api/v1/revies
+// @route   GET /api/v1/bootcamps/:bootcampId/reviews
+// @access  Public
+exports.getReviews = asyncHandler(async (req, res, next) => {
+
+    if (req.params.bootcampId) {
+        const reviews = await Review.find({ bootcamp: req.params.bootcampId });
+        return res.status(200).json({
+            success: true,
+            count: reviews.length,
+            data: reviews
+        });
+    } else {
+        res.status(200).json(res.advancedResults);
+    }
+});
+
