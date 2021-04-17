eaacc84c14f5575c05d3925382ad6e77bf53453a
jacques <jmarques1@gmail.com>
2021-04-17 08:29:41 -0300

    Get an sigle review

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
 const {
-    getReviews
+    getReviews,
+    getReview
 } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
@@ -17,5 +18,7 @@ router
         }),
         getReviews
     );
+
+router.route('/:id').get(getReview);
 
 module.exports = router;
